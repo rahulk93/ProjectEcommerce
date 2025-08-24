fd30d7fc36be4a197071d40d9241b6adec465b0e
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2025-08-25 00:46:51 +1200

    Committing and pushing the code at the sign-up error handling stage!

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,80 @@
+'use server';
+
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { signIn, signOut } from '@/auth';
+import { signInFormSchema, signUpFormSchema } from '../validator';
+import { hashSync } from 'bcrypt-ts-edge';
+import { prisma } from '@/db/prisma';
+
+
+// Sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+
+    await signIn('credentials', user);
+
+    return { success: true, message: 'Signed in successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: 'Invalid email or password' };
+  }
+}
+
+// Sign the user out
+export async function signOutUser() {
+  await signOut();
+}
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User registered successfully' };
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.code);
+    console.log(error.errors);
+    console.log(error.meta?.target)
+
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: 'User was not registered' };
+  }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,6 @@ export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split('.');
   return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
 }
+
+
+
