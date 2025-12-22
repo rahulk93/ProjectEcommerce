dd35a8a83e32ec43f58db229696854cb98c41227
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2025-12-23 09:12:36 +1300

    Search Filtering, Drawer & Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
 import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import { getLatestProducts, getFeaturedProducts } from '@/lib/actions/product.actions';
+import ProductCarousel from '@/components/shared/product/product-carousel';
+import ViewAllProductsButton from '@/components/view-all-products-button';
 
 const Homepage = async () => {
  const latestProducts = await getLatestProducts();
+ const featuredProducts = await getFeaturedProducts();
 
-  return <>
+  return (
+  <>
+  {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+  )}
   <ProductList data={latestProducts} title='Newest Arrivals' limit={4}/>
-  </>;
+  <ViewAllProductsButton />
+  </>
+  );
 }
  
 export default Homepage;
