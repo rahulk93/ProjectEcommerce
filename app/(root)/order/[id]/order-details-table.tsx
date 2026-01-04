07e766b8142729d3b3a17cbe2644b401aefc6da3
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-01-05 11:34:38 +1300

    Committing and pushing the code at Stripe Payment Success stage!

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -12,11 +12,12 @@ import { PayPalButtons, PayPalScriptProvider, usePayPalScriptReducer, } from '@p
 import { approvePayPalOrder, createPayPalOrder, deliverOrder, updateOrderToPaidCOD, } from '@/lib/actions/order.actions';
 import { toast } from "sonner";
 import { useTransition } from 'react';
+import StripePayment from './stripe-payment';
 
 
 
 
-const OrderDetailsTable = ({ order, paypalClientId, isAdmin, }: { order: Order, paypalClientId: string; isAdmin: boolean; }) => {
+const OrderDetailsTable = ({ order, paypalClientId, isAdmin, stripeClientSecret, }: { order: Order, paypalClientId: string; isAdmin: boolean; stripeClientSecret: string | null; }) => {
     const {
        id,
        shippingAddress,
@@ -209,6 +210,15 @@ const MarkAsDeliveredButton = () => {
                 </PayPalScriptProvider>
                 </div>
                 )}
+
+              {/* Stripe Payment */}
+              {!isPaid && paymentMethod === 'Stripe' && stripeClientSecret && (
+                <StripePayment
+                  priceInCents={Number(order.totalPrice) * 100}
+                  orderId={order.id}
+                  clientSecret={stripeClientSecret}
+                />
+              )}
 
                 { /* Cash On Delivery */}
                 {isAdmin && !isPaid && paymentMethod === 'CashOnDelivery' && (
