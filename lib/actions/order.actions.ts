0a35dfbfab3dc7c0639aa2d6496e6a20be77c2a7
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-01-05 14:59:10 +1300

    Stripe Payments

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -191,7 +191,7 @@ export async function approvePayPalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
